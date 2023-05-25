14a91af30911fa42ad8d1bfbb6ae1e713cdfcd6b
dhruv-patravali <dpatravali2022@francisparker.org>
2023-05-25 09:35:17 -0700

    Code for Lab6 (autograder)

--- grade.sh
+++ grade.sh
@@ -8,6 +8,22 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+cd student-submission
+if [[ -f ListExamples.java ]]
+then
+    echo File Found
+    cp ListExamples.java ../grading-area
+    cd ..
+    cp TestListExamples.java grading-area
+    cp -r lib grading-area
+    cd grading-area
+    javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+    java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+    ##java TestListExamples
+else 
+    echo File Not Found
+    exit
+fi
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
